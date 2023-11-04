33afd8b2e7e3461dd1dd651998f2aa82bad31cad
Prafull kumar <146866825+prafull370@users.noreply.github.com>
2023-11-04 18:12:24 +0530

    Add files via upload

---
+++ lowertriangle.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i-1;j++)
+        {
+            // printf(" ");
+        // }
+        // for(k=1;k<=6-i;k++)
+        // {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
