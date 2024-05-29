17a5f9ef1ec70cf99c01679ed75fd4219522be3b
Prafull Kumar <prafullkumar370@gmail.com>
2024-05-29 17:55:34 +0530

    new

---
+++ DSA/countingSort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void countingSort(int array[], int size) {
+  int output[10];
+
+  // Find the largest element of the array
+  int max = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] > max)
+      max = array[i];
+  }
+
+  // The size of count must be at least (max+1) but
+  // we cannot declare it as int count(max+1) in C as
+  // it does not support dynamic memory allocation.
+  // So, its size is provided statically.
+  int count[10];
+
+  // Initialize count array with all zeros.
+  for (int i = 0; i <= max; ++i) {
+    count[i] = 0;
+  }
+
+  // Store the count of each element
+  for (int i = 0; i < size; i++) {
+    count[array[i]]++;
+  }
+
+  // Store the cummulative count of each array
+  for (int i = 1; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
+
+  // Find the index of each element of the original array in count array, and
+  // place the elements in output array
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[array[i]] - 1] = array[i];
+    count[array[i]]--;
+  }
+
+  // Copy the sorted elements into original array
+  for (int i = 0; i < size; i++) {
+    array[i] = output[i];
+  }
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("\t%d  ", array[i]);
+  }
+}
+
+int main() {
+    int n;
+    printf("Enter your array size: ");
+    scanf("%d",&n);
+  int array[n];
+  printf("Enter your element: ");
+  for(int i=0;i<n;i++){
+  scanf("%d",&array[i]);
+  }
+//   int n = sizeof(array) / sizeof(array[0]);
+  countingSort(array, n);
+  printArray(array, n);
+}
